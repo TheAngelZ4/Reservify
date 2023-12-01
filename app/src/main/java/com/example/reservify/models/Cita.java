1bb0fca108547516e98b9f50795966ee358dbe70
AleCG0101 <131492006+AleCG0101@users.noreply.github.com>
2023-11-30 22:16:56 -0600

    commit

--- app/src/main/java/com/example/reservify/models/Cita.java
+++ app/src/main/java/com/example/reservify/models/Cita.java
@@ -1,5 +1,25 @@
 package com.example.reservify.models;
 
+import com.google.type.DateTime;
+
 public class Cita {
     private Integer id;
+    private String fecha;
+    private String hora;
+    private Integer id_negocio;
+    private String nombreNegocio;
+    private String direccionNegocio;
+    private String categoriaNegocio;
+    private Integer id_usuario;
+
+    public Cita(Integer id, String fecha, String hora, Integer id_negocio, String nombreNegocio, String direccionNegocio, String categoriaNegocio, Integer id_usuario){
+        this.id = id;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.id_negocio = id_negocio;
+        this.nombreNegocio = nombreNegocio;
+        this.direccionNegocio = direccionNegocio;
+        this.categoriaNegocio = categoriaNegocio;
+        this.id_usuario = id_usuario;
+    }
 }
---
+++ app/src/main/java/com/example/reservify/models/CitaResponse.java
@@ -0,0 +1,8 @@
+package com.example.reservify.models;
+
+public class CitaResponse {
+    public String mensaje;
+    public Cita response;
+
+    public CitaResponse(String mensaje)
+}
